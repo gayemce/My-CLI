9129203424b22e69b781f5635d2bd0699932b051
gayemce <100995170+gayemce@users.noreply.github.com>
2023-09-28 12:32:32 +0300

    updated

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -7,6 +7,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const yargs_1 = __importDefault(require("yargs"));
 const fs_1 = __importDefault(require("fs"));
 const child_process_1 = require("child_process");
+const readline_1 = __importDefault(require("readline")); //Konsolda soru sorulabilir ve yanıt yakalanabilir
+const rl = readline_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const argv = yargs_1.default
     .command({
     command: "create",
@@ -18,21 +23,16 @@ const argv = yargs_1.default
     }
 })
     .command("build", "Run npm run build")
+    .command("new", "Create a new project", {
+    'name': {
+        describe: "Project Name",
+        demandOption: true,
+        type: 'string',
+        alias: 'n'
+    }
+})
     .help()
     .argv;
-if (argv._.includes("create")) {
-    createStyleCss();
-}
-//exec komut satırında kodun çalıştırılmasını sağlar.
-if (argv._.includes("build")) {
-    (0, child_process_1.exec)("npm run build", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`Error: ${stdout}`);
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-}
 function createStyleCss() {
     const defaultCss = `
     *{
@@ -41,4 +41,51 @@ function createStyleCss() {
     }`;
     fs_1.default.writeFileSync("style.css", defaultCss);
     console.log("Style.css has been created.");
+}
+if (argv._.includes("create")) {
+    createStyleCss();
+}
+//exec komut satırında kodun çalıştırılmasını sağlar.
+if (argv._.includes("build")) {
+    (0, child_process_1.exec)("npm run build", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Error: ${stdout}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+}
+if (argv._.includes("new")) {
+    let projectName = argv.name;
+    //name değeri girilmezse
+    if (!projectName) {
+        rl.question("Please enter a project name:", (inputName) => {
+            projectName = inputName;
+            rl.close();
+            createNewProject(projectName);
+        });
+    }
+    else {
+        createNewProject(projectName);
+    }
+}
+function createNewProject(projectName) {
+    fs_1.default.mkdirSync(projectName);
+    (0, child_process_1.exec)(`git clone https://github.com/gayemce/CvProject-withApiMongodb.git ${projectName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${stdout}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        (0, child_process_1.exec)(`cd ${projectName} && npm install`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error: ${stdout}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+    });
 }
